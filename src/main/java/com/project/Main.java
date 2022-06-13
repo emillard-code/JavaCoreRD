91b23731fcfcbb7ed36bb327f735c66db54be26d
Emerson Millard <emillard.nt@gmail.com>
2022-06-12 21:51:19 -0700

    Initial commit

---
+++ src/main/java/com/project/Main.java
@@ -0,0 +1,45 @@
+package com.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(removeDuplicates("Hello"));
+        System.out.println(removeDuplicates("Kevin"));
+        System.out.println(removeDuplicates("Procrastinate"));
+
+    }
+
+    public static String removeDuplicates(String string) {
+
+        StringBuilder duplicateChars = new StringBuilder();
+
+        for (int x = 0; x < string.length(); x++) {
+
+            for (int y = x + 1; y < string.length(); y++) {
+
+                if (string.charAt(x) == string.charAt(y)) {
+                    if (!duplicateChars.toString().contains(Character.toString(string.charAt(x)))) { duplicateChars.append(string.charAt(x)); }
+                    break;
+                }
+
+            }
+
+        }
+
+        StringBuilder newString = new StringBuilder(string);
+
+        for (int x = 0; x < newString.length(); x++) {
+
+            if (duplicateChars.toString().contains(Character.toString(newString.charAt(x)))) {
+                newString.deleteCharAt(x);
+                x--;
+            }
+
+        }
+
+        return newString.toString();
+
+    }
+
+}
